b31f839ebbbcf3af5aa5a8b7d6b316087dd357b0
Eloi Alonso <eloi.alonso.7.4@gmail.com>
2021-10-25 16:01:20 +0200

    Add control over the image size

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ from tqdm import tqdm
 def main():
     args = parse_args()   
 
-    im_size = (256, 256)
+    im_size = (args.size, args.size)
     min_size = 0.1 * min(im_size)
     max_size = 0.3 * min(im_size)
     min_pos = 0 if args.can_go_out else max_size
     max_pos = min(im_size) if args.can_go_out else min(im_size) - max_size
 
     for i in tqdm(range(args.nb_images)):
-        fig, ax = plt.subplots()
+        fig = plt.figure(figsize=(im_size[0] + 1, im_size[1] + 1), dpi=1)
+        ax = plt.gca()
+        #fig, ax = plt.subplots(figsize=(64 / 100, 64 / 100))
         plt.axis("off")
         ax.imshow(np.ones((im_size[0], im_size[1], 3)))
 
@@ -37,12 +39,14 @@ def main():
                 shape = make_triangle(pos, size, angle, c)
             ax.add_patch(shape)
 
-        plt.savefig(f"images/image_{i:0{len(str(args.nb_images))}d}.png", bbox_inches="tight", pad_inches=0)
+        plt.tight_layout()
+        plt.savefig(f"images/image_{i:0{len(str(args.nb_images))}d}.png", bbox_inches="tight", pad_inches=0, dpi=1)
         plt.close()
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--size", type=int, default=64, help="Size of image.")
     parser.add_argument("-n", "--nb-images", type=int, default=10, help="Number of images to generate.")
     parser.add_argument("-o", "--can-overlap", action="store_true", help="Shapes can overlap.")
     parser.add_argument("-g", "--can-go-out", action="store_true", help="Shapes can go out of the image.")
